375d11bb04b3fb0ac2e929415c979eb8e21a6027
Dimacyber321 <b00144942@mytudublin.ie>
2023-04-17 12:03:08 +0100

    1

--- sql/init.sql
+++ sql/init.sql
@@ -9,3 +9,19 @@ CREATE TABLE users (
                        location VARCHAR(50),
                        date TIMESTAMP
 );
+
+use hotel;
+    create table booking(
+        id int(11) unsigned auto_increment primary key,
+        date timestamp,
+        number int(30) not null,
+        roomtype varchar(30) not null,
+        duration int(365) not null
+    );
+create table guest(
+    id int(11) unsigned auto_increment primary key,
+    userFname varchar(30) not null,
+    userLname varchar(30) not null,
+    userNum int(50) not null,
+    userAddress varchar(255) not null
+);
