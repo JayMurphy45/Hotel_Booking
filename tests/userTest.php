dbf81b085adcc3853a8aafb104f52759a2ba64a6
JayMurphy45 <113984910+JayMurphy45@users.noreply.github.com>
2023-04-18 13:18:08 +0100

    Update userTest.php

--- tests/userTest.php
+++ tests/userTest.php
@@ -5,11 +5,11 @@ $jamie = new Staff(1,"Jamie","Murphy","085-747-7464");
 
 $luke = new User(3,"31-4-21",5,2);
 
-$Dimitru = new Guest(5,"Dima","0856464252", "Pojoga");
+$Dumitru = new Guest(5,"Dima","0856464252", "Pojoga");
 
 
 echo $jamie;
 echo $luke;
-echo $Dimitru;
+echo $Dumitru;
 
 ?>
