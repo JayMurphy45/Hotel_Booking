0e0a3fe2508e9b401bf0d3325c7fc6c2062d12c9
LukeMurrin <113984774+LukeMurrin@users.noreply.github.com>
2023-04-24 15:40:48 +0100

    ,knlkl

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ if($_SESSION['Active'] == false){
     <center>
     <ul>
         <font size= 5>
-        <li><a href="../Booking.php">Booking</a></li>
+        <li><a href="Booking.php">Booking</a></li>
         <li><a href="CheckOut.php">Check Out</a></li>
         <li><a href="../LogIn.php">Login</a></li>
         <li><a href="../SignUp.php">Sign Up</a></li>
