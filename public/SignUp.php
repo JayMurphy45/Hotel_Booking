a580b80af8fa0a5a62fd096f324a3ac028b43a27
JayMurphy45 <113984910+JayMurphy45@users.noreply.github.com>
2023-04-24 15:41:11 +0100

    tests
    24/04/2023

--- public/SignUp.php
+++ public/SignUp.php
@@ -1,22 +1,19 @@
 <?php
 //create credentials
-if ($_SERVER['REQUEST_METHOD'] === ['POST']){
+if ($_SERVER['REQUEST_METHOD'] === ['POST']) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $confirmPassword = $_POST['confirm_password'];
 
     //validate form data
     $errors = [];
-    if (empty($username))
-    {
-        $errors[] ='Username is required';
+    if (empty($username)) {
+        $errors[] = 'Username is required';
     }
-    if (empty($password))
-    {
+    if (empty($password)) {
         $errors[] = 'Password is required';
     }
-    if ($password !== $confirmPassword)
-    {
+    if ($password !== $confirmPassword) {
         $errors[] = 'Passwords do not match';
     }
 
@@ -25,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === ['POST']){
         $file = 'users.txt';
         $data = $username . ':' . password_hash($password, PASSWORD_DEFAULT) . "\n";
         file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
-        header('Location: login.php');
+        header('Location: login.php'); //relocate to login.php
         exit;
     }
 }
@@ -33,34 +30,37 @@ if ($_SERVER['REQUEST_METHOD'] === ['POST']){
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Signup</title>
     <link rel="stylesheet" href="css/form.css">
 </head>
+
 <body>
-<!--- Print error if credentials are incorrect----->
-<?php if (isset($errors)) { ?>
-    <?php foreach ($errors as $error) { ?>
-        <div><?php echo $error; ?></div>
+    <!--- Print error if credentials are incorrect----->
+    <?php if (isset($errors)) { ?>
+        <?php foreach ($errors as $error) { ?>
+            <div><?php echo $error; ?></div>
+        <?php } ?>
     <?php } ?>
-<?php } ?>
 
     <h1>Sign Up</h1>
-<form method="post">
+    <form method="post">
 
-    <label>Username:</label>
-    <input type="text" name="username"><br>
+        <label>Username:</label>
+        <input type="text" name="username"><br>
 
-    <label>Password:</label>
-    <input type="password" name="password"><br>
+        <label>Password:</label>
+        <input type="password" name="password"><br>
 
-    <label>Confirm Password:</label>
-    <input type="password" name="confirm_password"><br>
+        <label>Confirm Password:</label>
+        <input type="password" name="confirm_password"><br>
 
-    <button type="submit">Sign Up</button>
-</form>
+        <button type="submit">Sign Up</button>
+    </form>
 
-<p><a href="../LogIn.php">Login</a></p>
+    <p><a href="../LogIn.php">Login</a></p>
 </body>
+
 </html>
---
+++ tests/unitTesting/bookingTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('public/Booking.php');
+
+$booking1 = new Booking(1, "12/2/23,6", 5);
+
+$booking2 = new Booking(1, "12/2/23,6", 4, "12345");
+
+$booking3 = new Booking(1, "12/2/23,6", 2, "12345",  5);
+
+echo $booking1;
+echo $booking2;
+echo $booking3;
