ebea5dc0c83d83b936f16cb8be018e9149ba415b
JayMurphy45 <113984910+JayMurphy45@users.noreply.github.com>
2023-04-26 10:40:16 +0100

    Update roomTest.php

--- tests/unitTesting/roomTest.php
+++ tests/unitTesting/roomTest.php
@@ -6,7 +6,7 @@ $room1 = new Room(45, 1, RoomType::SINGLE);
 
 $room2 = new Room(4, 5, RoomType::SUITE);
 
-$room3 = new Room(60, 2, RoomType::SUITE);
+$room3 = new Room(60, 2, RoomType::DOUBLE);
 
 echo $room1;
 echo $room2;
