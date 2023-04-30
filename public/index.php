95d1813f7346cd1a189722ef7d0ab68ff5592d06
LukeMurrin <113984774+LukeMurrin@users.noreply.github.com>
2023-04-30 21:00:38 +0100

    Moved index

---
+++ public/index.php
@@ -0,0 +1,46 @@
+<?php 
+//if($_SESSION['Active'] == false){
+//    header("location::login.php");
+//    exit;
+//}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Booking</title>
+    <link rel="stylesheet" href="public/css/form.css">
+    <link rel="stylesheet" href="public/css/navbar.css">
+</head>
+<body>
+    <?php
+        echo "<h1>Index Page</h1>";
+        require("Layouts/Header.php");
+    ?>
+    
+    <font size= 6>
+    <p>Here is the websites index:</p>
+    </font>
+    
+    <div class="div1">
+    <center>
+    <ul>
+        <font size= 5>
+        <li><a href="public/Booking.php">Booking</a></li>
+        <li><a href="public/CheckOut.php">Check Out</a></li>
+        <li><a href="LogIn.php">Login</a></li>
+        <li><a href="SignUp.php">Sign Up</a></li>
+        <li><a href="public/userProfile.php">Profile</a></li>
+        </font>
+        
+    </ul>
+    </center>
+    </div>
+</body>
+</html>
